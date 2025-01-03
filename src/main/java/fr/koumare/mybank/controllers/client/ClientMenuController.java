4b50ee4ef5aca84251bd351cc7ab10203e20d066
Koumare <ibrahimkoumare5@gmail.com>
2025-01-03 18:29:14 +0100

    Réalisation de l'interface menuClient

--- src/main/java/fr/koumare/mybank/controllers/client/ClientMenuController.java
+++ src/main/java/fr/koumare/mybank/controllers/client/ClientMenuController.java
@@ -1,4 +1,21 @@
 package fr.koumare.mybank.controllers.client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboard_btn;
+    public Button transaction_btn;
+    public Button accounts_btn;
+    public Button profile_btn;
+    public Button logoutt_btn;
+    public Button report_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
